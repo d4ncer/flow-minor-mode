00c035651d48187bbf0ea8dab4eac6a049178b2e
Raghuvir Kasturi <raghuvir.kasturi@gmail.com>
2018-12-12 08:55:02 +1300

    Add quiet & fix type-to-pos

--- flow-minor-mode.el
+++ flow-minor-mode.el
@@ -35,11 +35,6 @@
 
 (defconst flow-minor-buffer "*Flow Output*")
 
-(defconst flow-minor-garbage-error "Please wait. Server is garbage collecting shared memory: -")
-(defconst flow-minor-handle-error-1 "Please wait. Server is handling a request (starting up): \\")
-(defconst flow-minor-handle-error-2 "Please wait. Server is handling a request (starting up): -")
-(defconst flow-minor-handle-error-3 "Please wait. Server is handling a request (starting up)")
-
 (defcustom flow-minor-default-binary "flow"
   "Flow executable to use when no project-specific binary is found."
   :group 'flow-minor-mode
@@ -174,13 +169,8 @@ BODY progn"
    (let* ((file (buffer-file-name))
           (line (number-to-string (line-number-at-pos)))
           (col (number-to-string (1+ (current-column))))
-          (type (flow-minor-cmd-to-string "type-at-pos" "--from" "emacs" file line col)))
-     (message "%s" (flow-minor-colorize-type (s-chop-prefixes
-                                              (list flow-minor-garbage-error
-                                                    flow-minor-handle-error-1
-                                                    flow-minor-handle-error-2
-                                                    flow-minor-handle-error-3)
-                                              (car (s-lines type))))))))
+          (type (flow-minor-cmd-to-string "type-at-pos" "--quiet" "--from" "emacs" file line col)))
+     (message "%s" (flow-minor-colorize-type (car (split-string type "\n")))))))
 
 (defun flow-minor-jump-to-definition ()
   "Jump to definition."
